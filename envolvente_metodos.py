8bc4b0c085140147246f89151bafc657de7429b6
juan <dopplerjuan@gmail.com>
2018-09-01 13:41:13 -0300

    Saco ejemplo hilbert

--- envolvente_metodos.py
+++ envolvente_metodos.py
@@ -186,30 +186,6 @@ v_envelope = envelope_cabeza(v_filt, method='percentile', intervalLength=220,
                              perc=percentil)
 v_envelope = normalizar(v_envelope, minout=0, maxout=1)
 # %%
-duration = 1.0
-fs = 44150.0
-samples = int(fs*duration)
-t = np.arange(samples) / fs
-sig = signal.chirp(t, 100.0, t[-1], 300.0)
-sig *= signal.chirp(t, 5.0, t[-1], 30.0)
-sig *= signal.chirp(t, 5.0, t[-1], 30.0)
-# sig *= (1.0 + 0.5*np.sin(2.0*np.pi*60.0*t))
-
-analytic_signal = signal.hilbert(sig)
-amplitude_envelope = np.abs(analytic_signal)
-instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-instantaneous_frequency = np.diff(instantaneous_phase) / (2.0*np.pi) * fs
-fig = plt.figure()
-ax0 = fig.add_subplot(211)
-ax0.plot(t, sig, label='signal')
-ax0.plot(t, amplitude_envelope, label='envelope')
-ax0.set_xlabel("time in seconds")
-ax0.legend()
-ax1 = fig.add_subplot(212)
-ax1.plot(t[1:], instantaneous_frequency)
-ax1.set_xlabel("time in seconds")
-ax1.set_ylim(0.0, 120.0)
-# %%
 fig, ax = plt.subplots(2, figsize=(10, 5), sharex=True)
 hil_env = np.abs(signal.hilbert(v_filt))
 

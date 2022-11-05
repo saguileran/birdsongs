3b2c51a95c0bb7c3b20b24f872d494f1d8cde0b2
Sebastian Aguilera Novoa <42812846+saguileran@users.noreply.github.com>
2022-11-05 17:03:25 -0500

    update

---
+++ birdsongs/.ipynb_checkpoints/paths-checkpoint.py
@@ -0,0 +1,18 @@
+import os, glob
+
+class Paths(object):
+    def __init__(self, root_path, bird_name=""):
+        self.root     = root_path
+        self.base     = "{}birdsongs\\".format(self.root) 
+        self.auxdata  = '{}auxiliar_data\\'.format(self.base)
+        self.results  = '{}results\\'.format(self.base) 
+        self.audios   = '{}audios\\'.format(self.root)
+        self.examples = '{}examples\\'.format(self.audios)
+
+        if not os.path.exists(self.results):    os.makedirs(self.results)
+        if not os.path.exists(self.examples):    os.makedirs(self.examples)
+
+        self.sound_files   = glob.glob(os.path.join(self.audios, '*wav')) #'*'+birdname+'*wav' 
+        self.no_files      = len(self.sound_files)
+        
+        print("The folder has {0} songs".format(self.no_files))

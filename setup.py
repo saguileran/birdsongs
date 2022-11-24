8716ab9486df71dfcd106b60cd3602153cd4aa0a
Sebastian Aguilera Novoa <42812846+saguileran@users.noreply.github.com>
2022-11-24 01:18:40 -0500

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,35 @@ from setuptools import setup
 
 
 if __name__ == '__main__':
-    setup()
+    setup(
+    name='birdsongs',
+    version='0.1.0',
+    author='Sebastian Aguilera Novoa',
+    author_email='saguileran@unal.edu.co',
+    packages=['birdsongs'],
+    url='https://github.com/saguileran/birdsongs',
+    license='LICENSE.txt',
+    description='A python package to analyze and visualize birdsongs',
+    long_description=open('README.txt').read(),
+    install_requires=[
+            "librosa",
+            "lmfit",
+            "scipy",
+            "sympy",
+            "numpy",
+            "pandas",
+            "matplotlib",
+            "playsound",
+            "PeakUtils",
+            "mpl_pan_zoom",
+            "mpl_point_clicker",
+            "scikit_learn",
+            "scikit_maad",
+            "setuptools",
+            "ipython",
+            "lazy_loader",
+    ],
+        )
     
     # Allow editable install into user site directory.
     # See https://github.com/pypa/pip/issues/7953.

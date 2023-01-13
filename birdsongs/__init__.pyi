1312cd3838f34d91a15580ada3f07b03402061b4
Sebastian Aguilera Novoa <42812846+saguileran@users.noreply.github.com>
2023-01-13 11:41:36 -0500

    update branch

--- birdsongs/__init__.pyi
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import utils  
-from .ploter import Ploter
-from .paths import Paths
-from .birdsong import BirdSong
-from .syllable import Syllable
-from .optimizer import Optimizer
-
-from .version import show_versions as show_versions
